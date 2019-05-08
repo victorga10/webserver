a3e0934e180dabd5cb9bb042158f230fcbfcaa19
VictorAndrade14 <victor@serverti.com.br>
2019-05-08 16:29:42 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 ln -sf /dev/stdout /var/log/httpd/access_log
 ln -sf /dev/stderr /var/log/httpd/error_log
 
-chown -R apache:apache /var/www/ && chmod -R 770 /var/www/
+chown -R apache:apache /var/www/ && chmod -R 777 /var/www/
 
 
 set -e
